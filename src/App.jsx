4156c0f8c2b359c4e9112bd6bf1c78fd0d17fec2
RafaelPA13 <rafaporann@gmail.com>
2024-10-03 17:52:54 -0300

    Finalização da home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
-import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="flex">
+    <div className="flex w-full">
       <Header />
-      <div className="p-7 text-2xl font-semibold flex-1 h-screen">
+      <div className="w-full">
         <Outlet />
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,25 @@
 export default function Home() {
-    return (
-        <>
-            <h1>Home</h1>
-        </>
-    )
+  return (
+    <div className="h-screen bg-gradient-to-r from-gray-300 flex items-center">
+      <div className="w-full px-6 lg:flex lg:items-center lg:justify-between">
+        <div className="lg:w-1/2">
+          <h4 className="font-medium text-gray-800">Olá, Sou Rafael 👋</h4>
+          <h1 className="text-5xl font-bold text-blue-900 mt-2">
+            Engenheiro de Software
+          </h1>
+          <p className="text-gray-600 mt-4">
+            Sou um engenheiro de software e estou pronto para tranforma a sua
+            ideia em realidade.
+          </p>
+        </div>
+        <div class="mt-6 lg:mt-0 lg:w-1/2">
+          <img
+            src="/Perfil.jpg"
+            alt="Luca"
+            className="w-full h-auto rounded-lg lg:h-svh"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
